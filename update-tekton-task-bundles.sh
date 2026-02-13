f48ad4ef2d6f24935aeb2f37e0bbb32013a15312
Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
2026-02-13 06:42:59 -0500

    feat: add YAML linting and update linting logic (#886)
    * feat: populate image labels from bundle metadata
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
    
    * chore: update YAML for linting rules
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
    
    * feat: add linting workflow
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
    
    * chore: shellcheck update task script
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Dale Haiducek <19750917+dhaiducek@users.noreply.github.com>

--- update-tekton-task-bundles.sh
+++ update-tekton-task-bundles.sh
@@ -2,7 +2,7 @@
 
 # Use this script to update the Tekton Task Bundle references used in a Pipeline or a PipelineRun.
 # update-tekton-task-bundles.sh .tekton/*.yaml
-# The script is copied and modified from https://konflux-ci.dev/docs/troubleshooting/builds/#:~:text=You%20can%20find%20the%20newest,For%20example
+# The script is copied and modified from https://konflux-ci.dev/docs/troubleshooting/builds/#manually-update-task-bundles
 
 set -euo pipefail
 
@@ -13,11 +13,11 @@ else
   SED_INPLACE=(-i)
 fi
 
-FILES=$@
+FILES=( "$@" )
 
 # Find existing image references
 OLD_REFS="$(\
-    yq '... | select(has("resolver")) | .params // [] | .[] | select(.name == "bundle") | .value'  $FILES | \
+    yq '... | select(has("resolver")) | .params // [] | .[] | select(.name == "bundle") | .value'  "${FILES[@]}" | \
     grep -v -- '---' | \
     sort -u \
 )"
@@ -32,31 +32,31 @@ for old_ref in ${OLD_REFS}; do
     old_tag="${repo_tag##*:}"
     old_digest="${old_ref##*@}"
 
-    tags=$(skopeo list-tags docker://${repo} | yq '.Tags[]' | tr -d '"')
+    tags=$(skopeo list-tags "docker://${repo}" | yq '.Tags[]' | tr -d '"')
 
     main_tags=$(echo "$tags" | grep -E '^[0-9]+(\.[0-9]+)*$')
     latest_main_tag=$(echo "$main_tags" | sort -V | tail -n1)
 
     if [[ "$old_tag" != "$latest_main_tag" ]]; then
-        task_name=$(basename $repo)
+        task_name=$(basename "${repo}")
         task_name=${task_name#task-}
 
         # Get new digest for the latest tag
-        new_digest=$(skopeo inspect docker://${repo}:${latest_main_tag} | yq '.Digest' | tr -d '"')
+        new_digest=$(skopeo inspect "docker://${repo}:${latest_main_tag}" | yq '.Digest' | tr -d '"')
 
         # Find which files contain this reference
-        for file in $FILES; do
-            if grep -q "$old_ref" "$file" 2>/dev/null; then
+        for file in "${FILES[@]}"; do
+            if grep -q "${old_ref}" "${file}" 2>/dev/null; then
                 # Create JSON object for this migration
                 migration_entry=$(cat <<EOF
   {
-    "depName": "$repo",
+    "depName": "${repo}",
     "link": "https://github.com/konflux-ci/build-definitions/tree/main/task/${task_name}",
-    "currentValue": "$old_tag",
-    "currentDigest": "$old_digest",
-    "newValue": "$latest_main_tag",
-    "newDigest": "$new_digest",
-    "packageFile": "$file",
+    "currentValue": "${old_tag}",
+    "currentDigest": "${old_digest}",
+    "newValue": "${latest_main_tag}",
+    "newDigest": "${new_digest}",
+    "packageFile": "${file}",
     "parentDir": ".",
     "depTypes": ["tekton-bundle"]
   }
@@ -67,19 +67,19 @@ EOF
         done
     fi
 
-    new_digest=$(skopeo inspect docker://${repo}:${old_tag} | yq '.Digest')
+    new_digest=$(skopeo inspect "docker://${repo}:${old_tag}" | yq '.Digest')
     new_ref="${repo}:${old_tag}@${new_digest}"
-    for file in $FILES; do
+    for file in "${FILES[@]}"; do
         sed "${SED_INPLACE[@]}" -e "s!${old_ref}!${new_ref}!g" "$file"
     done
 done
 
 # Output migration data in JSON format
-if [ ${#migration_data[@]} -gt 0 ]; then
+if [[ ${#migration_data[@]} -gt 0 ]]; then
     echo "["
     for i in "${!migration_data[@]}"; do
         echo "${migration_data[$i]}"
-        if [ $i -lt $((${#migration_data[@]} - 1)) ]; then
+        if [[ $i -lt $((${#migration_data[@]} - 1)) ]]; then
             echo ","
         fi
     done
